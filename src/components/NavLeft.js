af4b9a5b0d4a4d3dd71bc6ea32eefcb65c606316
Igorg599 <igorg599@gmail.com>
2021-02-26 23:27:32 +0500

    commit-3

--- src/components/NavLeft.js
+++ src/components/NavLeft.js
@@ -4,26 +4,99 @@ import axios from 'axios'
 
 const NavLeft = () => {
     const [items, setItems] = React.useState(null)
+    const [filterItems, setFilterItems] = React.useState(null)
+    const [filterItemsPopUp, setFilterItemsPopUp] = React.useState(null)
+    const [visiblePopup, setVisiblePopup] = React.useState(false)
+    const sortRef = React.useRef()
 
     React.useEffect(() => {
         axios.get('/src/db.json/items').then ((data) => {
             setItems(data.data)
-        });
-    }, [])
+        })
+        if (document.documentElement.clientWidth >= 1014) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 10))
+            setFilterItemsPopUp(null)
+        }
+        if (document.documentElement.clientWidth < 1014) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 9))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id === 9))
+        }
+        if (document.documentElement.clientWidth < 926) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 8))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10 && item.id > 7))
+        }
+        if (document.documentElement.clientWidth < 838) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 7))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10 && item.id > 6))
+        }
+        if (document.documentElement.clientWidth < 750) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 6))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10 && item.id > 5))
+        }
+        if (document.documentElement.clientWidth < 662) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 5))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10 && item.id > 4))
+        }
+        if (document.documentElement.clientWidth < 574) {
+            setFilterItems(items && items.iconsleftmenu.filter(item => item.id < 4))
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10 && item.id > 3))
+        }
+        if (document.documentElement.clientWidth < 511) {
+            setFilterItems(null)
+            setFilterItemsPopUp(items && items.iconsleftmenu.filter(item => item.id < 10))
+        }
+    }, [items])
+
+    const handleOutsideClick = (e) => {
+        const path = e.path || (e.composedPath && e.composedPath());
+        if (!path.includes(sortRef.current)) {
+            setVisiblePopup(false);
+        }
+    };
+
+    React.useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick)
+    }, []);
+
     return (
         <div className='navleft'>
             <div className='navleft__logo'>
                 {items && <img width={items.logo.width} src={items.logo.src} alt='dsd'/>}
                 <p>{items && items.logo.name}</p>
             </div>
-            {items && 
-                items.iconsleftmenu.map(item => (
+            {filterItems && 
+                filterItems.map(item => (
                     <div className='navleft__icons' key={item.id}>
                         <img width={item.width} src={item.src} alt='icons'/>
                         <p>{item.name}</p>
                     </div> 
-                ))
+                )) 
             }
+            {items && 
+                <div ref={sortRef} className='navleft__more' onClick={() => setVisiblePopup(true)}>
+                    <img width={items.iconsleftmenu[9].width} src={items.iconsleftmenu[9].src} alt='icons'/>
+                    <p>{items.iconsleftmenu[9].name}</p>
+                </div>
+            }
+            {items && 
+                <div ref={sortRef} className='navleft__gamburger' onClick={() => setVisiblePopup(true)}>
+                    <img width={items.gamburger.width} src={items.gamburger.src} alt='icons'/>
+                    <div className='navleft__gamburger_label'/>
+                </div>
+            }
+            {visiblePopup && <div className="navleft__popup">
+                <ul>
+                    {filterItemsPopUp &&
+                        filterItemsPopUp.map((item, index) => (
+                            <li onClick={() => setVisiblePopup(false)}  
+                            key={index}>
+                                <img key={item.id} width={item.width} src={item.src} alt='icons'/>
+                                <p>{item.name}</p>      
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>}
         </div>
     )
 }
--- src/components/NavRight.js
+++ src/components/NavRight.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const NavRight = () => {
     const [items, setItems] = React.useState(null)
+    const [visiblePopup, setVisiblePopup] = React.useState(false);
+    const sortRef = React.useRef();
 
     React.useEffect(() => {
         axios.get('/src/db.json/items').then ((data) => {
@@ -10,17 +12,50 @@ const NavRight = () => {
         });
     }, [])
 
+    const handleOutsideClick = (e) => {
+        const path = e.path || (e.composedPath && e.composedPath());
+        if (!path.includes(sortRef.current)) {
+            setVisiblePopup(false);
+        }
+    };
+
+    React.useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick)
+    }, []);
+
+
     return (
         <div className='navright'>
             <div className='navright__coin'>
-                {items && <img width={items.iconsrightmenu[0].width} src={items.iconsrightmenu[0].src} alt='dsd'/>}
+                {items && <img width={items.iconsrightmenu[0].width} src={items.iconsrightmenu[0].src} alt='icon'/>}
                 <p>{items && items.iconsrightmenu[0].name}</p>
             </div>
             <div className='navright__menu'>
                 {items && items.iconsrightmenu.filter(item => item.id > 1).map(item => (
-                    <img width={item.width} src={item.src} alt='icons'/>
+                    <img key={item.id} width={item.width} src={item.src} alt='icons'/>
                 ))}
             </div>
+            <div ref={sortRef} className='navright__gamburger' onClick={() => setVisiblePopup(true)}>
+                {items && 
+                    <>
+                        <img width={items.iconsrightmenu[2].width} src={items.iconsrightmenu[2].src} alt='icon'/>
+                        <div className='navright__gamburger_label'/>
+                    </>
+                }
+            </div>
+            {visiblePopup && <div className="navright__popup">
+                <ul>
+                    {items &&
+                        items.iconsrightmenu.map((item, index) => (
+                            <li onClick={() => setVisiblePopup(false)}  
+                            key={index}>
+                                <img key={item.id} width={item.width} src={item.src} alt='icons'/>
+                                <p>{item.name}</p>      
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>}
         </div>
     )
 }
